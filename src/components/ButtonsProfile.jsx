772815aad88a71d962cac83f9c0b28427c54adb8
Perr0fe0 <83041787+Perr0fe0@users.noreply.github.com>
2024-05-12 23:29:58 -0400

    Diseño base

---
+++ src/components/ButtonsProfile.jsx
@@ -0,0 +1,8 @@
+import './css/ButtonsProfile.css'
+
+const ButtonsProfile = (props) =>{
+    return(
+        <button className='btn'>{props.text}</button>
+    )
+}
+export default ButtonsProfile
---
+++ src/components/CardProfile.jsx
@@ -0,0 +1,32 @@
+import './css/Card.css'
+import Images from '../assets/images/avatar-jessica.jpeg'
+import ButtonsProfile from "./ButtonsProfile"
+const CardProfile = () => {
+    return (
+        <>
+
+            <div className='card'>
+                <div className='img'>
+                    <img src={Images} alt="" />
+                </div>
+
+                <div className='info'>
+                    <h1>Jessica Randall</h1>
+                    <h2>London, United Kingdom</h2>
+                    <p>"Front-end developer and avid reader."</p>
+                </div>
+                <div className='buttons'>
+                    <ButtonsProfile text='Github' />
+                    <ButtonsProfile text='Fronted Mentor' />
+                    <ButtonsProfile text='Linkedin' />
+                    <ButtonsProfile text='Twitter' />
+                    <ButtonsProfile text='Instagram' />
+                </div>
+            </div>
+
+
+        </>
+    )
+}
+
+export default CardProfile
